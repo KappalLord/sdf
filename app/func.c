10401350219dd5b38011fb04b20bf60d9b91644b
KappalLord <78732975+KappalLord@users.noreply.github.com>
2021-04-05 13:04:49 +0300

    Add files via upload

---
+++ app/func.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include "func.h"
+
+solution Func(double a, double b, double c) {
+    solution res;
+
+    if(a == 0){
+        if(b == 0){
+            res.count = 0;
+            return res;
+        }else{
+        res.x1 = (-c)/b;
+        res.count = 1;
+        return res;
+        }
+    }
+
+    double D = pow(b, 2) - 4*a*c;
+
+    if(D < 0.0) {
+        res.count = 0;
+
+    }
+    if(D == 0.0) {
+        res.count = 1;
+        res.x1 = -b/(2*a);
+    }
+    if(D > 0.0) {
+        res.count = 2;
+        res.x1 = (-b - sqrt(D)) / (2*a);
+        res.x2 = (-b + sqrt(D)) / (2*a);
+    }
+
+    return res;
+}
